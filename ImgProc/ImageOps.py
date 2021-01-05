5aa9430ef340a897117db1faaafc20da7447ddf2
Tong Shao <tong__shao@outlook.com>
2021-01-05 19:55:50 +0800

    new function: add_gaussian_noise / add_salt_pepper_noise

--- ImgProc/ImageOps.py
+++ ImgProc/ImageOps.py
@@ -1,8 +1,11 @@
 import numpy as np
+import random
 from . import Image
 
 
 def equalize(image):
+    if not isinstance(image, Image.Image):
+        raise TypeError
     if image.mode is not Image.COLOR_L:
         raise NotImplementedError
     h, w = image.size
@@ -20,3 +23,38 @@ def equalize(image):
     new.im = hists_cdf[im]
 
     return new
+
+
+def add_gaussian_noise(image, mean=0, var=0.01):
+    if not isinstance(image, Image.Image):
+        raise TypeError
+
+    new = image.copy()
+    im = np.array(new.im / 255, dtype=float)
+    noise = np.random.normal(mean, var ** 0.5, im.shape)
+    im = np.uint8(np.clip(im + noise, 0, 1) * 255)
+    new.im = im
+
+    return new
+
+
+def add_salt_pepper_noise(image, prob=0.01):
+    if not isinstance(image, Image.Image):
+        raise TypeError
+
+    thres = 1 - prob
+    new = image.copy()
+    im = new.im
+    for i in range(im.shape[0]):
+        for j in range(im.shape[1]):
+            rand = random.random()
+            if rand < prob:
+                im[i][j] = 0
+            elif rand > thres:
+                im[i][j] = 255
+    new.im = im
+
+    return new
+
+
+
--- test.py
+++ test.py
@@ -3,11 +3,17 @@ from PIL import Image as PILImage
 from ImgProc import Image, ImageOps
 
 if __name__ == '__main__':
-    filename = "data/lena.bmp"
+    filename = "data/img_100.bmp"
     img = Image.from_bmp_file(filename)
-    # print(img.get_pixel((0, 0)))
-    img.convert(Image.COLOR_L)
     img.show()
-    img_eq = ImageOps.equalize(img)
-    img_eq.show()
-    img_eq.show_histogram()
+    # print(img.get_pixel((0, 0)))
+    # img.convert(Image.COLOR_L)
+    # img.show()
+    # img.show_histogram()
+    # img_eq = ImageOps.equalize(img)
+    # img_eq.show()
+    # img_eq.show_histogram()
+    img_w_gn = ImageOps.add_gaussian_noise(img)
+    img_w_gn.show()
+    img_w_spn = ImageOps.add_salt_pepper_noise(img)
+    img_w_spn.show()
