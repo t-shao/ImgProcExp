756b08732c961ee090414b4d83b584f07481d8db
Tong Shao <tong__shao@outlook.com>
2021-01-15 16:35:21 +0800

    add Kernel class to support template filter (convolution) add Sobel, Prewitt, Robert(not work yet), Laplacian operators rearrange the test script

--- ImgProc/ImageFilter.py
+++ ImgProc/ImageFilter.py
@@ -1,3 +1,4 @@
+import functools
 import numpy as np
 
 
@@ -66,4 +67,130 @@ class MeanFilter(Filter):
         return new
 
 
-# TODO: template operator
+class Kernel(object):
+    """
+        Create a convolution kernel.
+
+        :param size: Kernel size.
+        :param kernel: A numpy array containing kernel weights.
+        :param scale: Scale factor. If given, the result for each pixel is
+                        divided by this value.  The default is the sum of the
+                        kernel weights.
+        :param offset: Offset. If given, this value is added to the result,
+                        after it has been divided by the scale factor.
+    """
+    def __init__(self, size, kernel, scale=None, offset=0):
+        if scale is None:
+            # default scale is sum of kernel
+            scale = np.sum(kernel) if np.sum(kernel) != 0 else 1
+        if kernel.shape != size:
+            raise ValueError
+        self.filterargs = size[0], scale, offset, kernel
+
+    @property
+    def size(self):
+        return self.filterargs[0]
+
+    @property
+    def scale(self):
+        return self.filterargs[1]
+
+    @property
+    def offset(self):
+        return self.filterargs[2]
+
+    @property
+    def kernel(self):
+        return self.filterargs[3]
+
+    def _convolve(self, im):
+        kernel = self.kernel
+        kernel_size = self.size
+
+        h = int(im.shape[0])
+        w = int(im.shape[1])
+        # padding
+        padding = int((kernel_size - 1) / 2)
+        padded_h = int(h + 2 * padding)
+        padded_w = int(w + 2 * padding)
+
+        im_padded = np.zeros((padded_h, padded_w))
+        im_padded[padding:padding + h, padding:padding + w] = im[:, :]
+
+        im_res = np.zeros(im.shape)
+        for i in range(padding, padding + h):
+            for j in range(padding, padding + w):
+                im_res[i - padding][j - padding] = np.sum(
+                    im_padded[i - padding:i + padding + 1, j - padding:j + padding + 1] * kernel, dtype=np.uint8)
+
+        return im_res
+
+    def filter(self, image):
+        im = image.im
+        new = image.copy()
+        new.im = np.uint8(self._convolve(im) / self.scale + self.offset)
+
+        return new
+
+
+class Sobel(Kernel):
+    def __init__(self, mode):
+        if mode not in ['v', 'h']:
+            raise ValueError
+
+        size = (3, 3)
+        if mode == 'v':
+            kernel = np.array([[-1,  0,  1],
+                               [-2,  0,  2],
+                               [-1,  0,  1]])
+        else:
+            kernel = np.array([[-1, -2, -1],
+                               [ 0,  0,  0],
+                               [ 1,  2,  1]])
+
+        super().__init__(size, kernel)
+
+
+class Prewitt(Kernel):
+    def __init__(self, mode):
+        if mode not in ['v', 'h']:
+            raise ValueError
+
+        size = (3, 3)
+        if mode == 'v':
+            kernel = np.array([[-1,  0,  1],
+                               [-1,  0,  1],
+                               [-1,  0,  1]])
+        else:
+            kernel = np.array([[-1, -1, -1],
+                               [ 0,  0,  0],
+                               [ 1,  1,  1]])
+
+        super().__init__(size, kernel)
+
+
+# TODO: Robert operator is not properly functional yet
+class Robert(Kernel):
+    def __init__(self, mode):
+        if mode not in [45, 135]:
+            raise ValueError
+
+        size = (2, 2)
+        if mode == 45:
+            kernel = np.array([[ 1,  0],
+                               [ 0, -1]])
+        else:
+            kernel = np.array([[ 0,  1],
+                               [-1,  0]])
+
+        super().__init__(size, kernel)
+
+
+class Laplacian(Kernel):
+    def __init__(self):
+        size = (3, 3)
+        kernel = np.array([[ 0, -1,  0],
+                           [-1,  4, -1],
+                           [ 0, -1,  0]])
+
+        super().__init__(size, kernel)
--- test.py
+++ test.py
@@ -4,30 +4,66 @@ from matplotlib import pyplot as plt
 from ImgProc import Image, ImageOps, ImageFilter, Transform
 
 if __name__ == '__main__':
+    '''read file'''
     filename = "data/img_100.bmp"
     img = Image.from_bmp_file(filename)
+
+    '''show the image'''
     # plt.imshow(img.im)
     # plt.show()
     # img.show()
+
+    ''' get pixel '''
     # print(img.get_pixel((0, 0)))
+
+    '''convert to monochrome'''
     img.convert(Image.COLOR_L)
+    img.show()
+
+    '''crop into a patch'''
     # img_se = img.crop((0, 0, 32, 32))
     # img_se.show()
-    # img.show()
+
+    '''show and equalize the histogram'''
     # img.show_histogram()
     # img_eq = ImageOps.equalize(img)
     # img_eq.show()
     # img_eq.show_histogram()
+
+    '''add gaussian noise'''
     # img_w_gn = ImageOps.add_gaussian_noise(img)
     # img_w_gn.show()
-    img_w_spn = ImageOps.add_salt_pepper_noise(img)
-    img_w_spn.show()
-    median_filter = ImageFilter.MedianFilter(3)
-    mean_filter = ImageFilter.MeanFilter(3)
-    img_dn_med = median_filter.filter(img_w_spn)
-    img_dn_med.show()
-    img_dn_mean = mean_filter.filter(img_w_spn)
-    img_dn_mean.show()
+
+    '''add salt & pepper noise'''
+    # img_w_spn = ImageOps.add_salt_pepper_noise(img)
+    # img_w_spn.show()
+
+    '''apply the median filter'''
+    # median_filter = ImageFilter.MedianFilter(3)
+    # img_dn_med = median_filter.filter(img_w_spn)
+    # img_dn_med.show()
+
+    '''apply the mean filter'''
+    # mean_filter = ImageFilter.MeanFilter(3)
+    # img_dn_mean = mean_filter.filter(img_w_spn)
+    # img_dn_mean.show()
+
+    '''apply the sobel operator'''
+    sobel_v = ImageFilter.Sobel('v')
+    img_sobel_v = sobel_v.filter(img)
+    img_sobel_v.show()
+
+    '''apply the robert operator'''
+    # robert_45 = ImageFilter.Robert(45)
+    # img_rob = robert_45.filter(img)
+    # img_rob.show()
+
+    '''apply the laplacian operator'''
+    laplacian = ImageFilter.Laplacian()
+    img_lap = laplacian.filter(img)
+    img_lap.show()
+
+    '''dft'''
     # img_freq = Transform.dft2(img_se)
     # plt.imshow(abs(img_freq))
     # plt.show()
